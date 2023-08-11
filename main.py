7f7d3ff29339b12bbf8cf807402a5741baf6f008
sarthaklambaa <samlamba29@gmail.com>
2023-08-11 21:10:32 +0530

    Fixed alarm tune

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
-import datetime
 import time
 from pygame import mixer
-import threading
 
 root = Tk()
 root.title("Alarm Clock")
@@ -19,24 +17,26 @@ msginp = StringVar()
 
 mixer.init()
 
-head = Label(root, text="Alarm Clock", font=('comic sans', 20))
-head.grid(row=0, column=1, columnspan=2)
+head = Label(root, text="Alarm Clock", font=('Helvetica', 20, 'bold'))
+head.grid(row=0, column=1, columnspan=2, pady=10)
+
 
 def ala():
-    a = alarmtime.get()
-    AlarmT = a
-    CurrentTime = time.strftime('%H:%M')
-
-    while AlarmT != CurrentTime:
+    confirm = messagebox.askquestion("Set Alarm", "Do you want to set the alarm?")
+    if confirm == 'yes':
+        a = alarmtime.get()
+        AlarmT = a
         CurrentTime = time.strftime('%H:%M')
 
-    if AlarmT == CurrentTime:
-        msg = messagebox.showinfo("Info", "f{msginp.get()}")
-        mixer.music.load("alarm.mp3")
-        mixer.music.play()
-        if msg == 'yes':
-            mixer.stop()
+        while AlarmT != CurrentTime:
+            CurrentTime = time.strftime('%H:%M')
 
+        if AlarmT == CurrentTime:
+            mixer.music.load("alarm.mp3")
+            mixer.music.play()
+            msg = messagebox.showinfo("Info", f"Alarm: {msginp.get()}")
+            if msg == 'ok':
+                mixer.stop()
 
 original_image = Image.open("clock.png")
 small_image = original_image.resize((150, 150))
@@ -45,19 +45,19 @@ clock_img = ImageTk.PhotoImage(small_image)
 img = Label(root, image=clock_img)
 img.grid(row=1, rowspan=4, column=0, padx=20)
 
-input_time = Label(root, textvariable='alarmtime', text="Input Time", font=('comic sans', 18))
-input_time.grid(row=1, column=1, columnspan=2, sticky="w")
+input_time = Label(root, text="Input Time", font=('Helvetica', 18))
+input_time.grid(row=1, column=1, columnspan=2, sticky="w", pady=5)
 
-alarm_time = Entry(root, font=('comic sans', 18), width=6)
-alarm_time.grid(row=1, column=3, sticky="w")
+alarm_time = Entry(root, textvariable=alarmtime, font=('Helvetica', 18), width=8)
+alarm_time.grid(row=1, column=3, sticky="w", pady=5)
 
-msg = Label(root, text="Message", font=('comic sans', 18))
-msg.grid(row=2, column=1, columnspan=2, sticky="w")
+msg = Label(root, text="Message", font=('Helvetica', 18))
+msg.grid(row=2, column=1, columnspan=2, sticky="w", pady=5)
 
-msg_input = Entry(root, textvariable='msginp', font=('comic sans', 18), width=8)
-msg_input.grid(row=2, column=3, columnspan=2, sticky="w")
+msg_input = Entry(root, textvariable=msginp, font=('Helvetica', 18), width=15)
+msg_input.grid(row=2, column=3, columnspan=2, sticky="w", pady=5)
 
-submit = Button(root, text="Set Alarm", font=('comic sans', 18))
-submit.grid(row=3, column=1, columnspan=2, sticky="w")
+submit = Button(root, text="Set Alarm", font=('Helvetica', 18), command=ala)
+submit.grid(row=3, column=1, columnspan=2, sticky="w", pady=10)
 
 root.mainloop()
